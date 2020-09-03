e7773d4ee8e0fa455f2c21b75f04afe89d34874c
natasajevtic <nataly.jevtic.95@gmail.com>
2020-09-03 12:26:07 +0200

    Add classes for validation

---
+++ DAN_LX_Natasa_Jevtic/Zadatak_1/Validations/Wrapper.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows;
+
+namespace Zadatak_1.Validations
+{
+    class Wrapper : DependencyObject
+    {
+        public static readonly DependencyProperty OldIdCardNumberProperty =
+        DependencyProperty.Register("OldIdCardNumber", typeof(string),
+        typeof(Wrapper), new PropertyMetadata(string.Empty));
+
+        public string OldIdCardNumber
+        {
+            get { return (string)GetValue(OldIdCardNumberProperty); }
+            set { SetValue(OldIdCardNumberProperty, value); }
+        }
+
+        public static readonly DependencyProperty OldPhoneNumberProperty =
+        DependencyProperty.Register("OldPhoneNumber", typeof(string),
+        typeof(Wrapper), new PropertyMetadata(string.Empty));
+
+        public string OldPhoneNumber
+        {
+            get { return (string)GetValue(OldPhoneNumberProperty); }
+            set { SetValue(OldPhoneNumberProperty, value); }
+        }
+
+        public static readonly DependencyProperty OldJmbgProperty =
+        DependencyProperty.Register("OldJmbg", typeof(string),
+        typeof(Wrapper), new PropertyMetadata(string.Empty));
+
+        public string OldJmbg
+        {
+            get { return (string)GetValue(OldJmbgProperty); }
+            set { SetValue(OldJmbgProperty, value); }
+        }
+    }
+}
